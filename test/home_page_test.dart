32b5e113630a31cb7bf9aa7c3aa90a0c5576c10d
Morgan Lyddon <lyddon.morg@gmail.com>
2022-06-02 23:36:25 -0500

    Refactored Pages
    added pages directory

--- test/home_page_test.dart
+++ test/home_page_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:pet_healh_monitor/home_page.dart';
+import 'package:pet_healh_monitor/pages/home_page.dart';
 
 void main() {
   group('Home Page Layout Tests', () {
